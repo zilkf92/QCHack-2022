f8a1fe92062fe82dd8f6bd95a5e3bbdf1e7c6feb
Felix Zilk <zilk.felix@gmail.com>
2022-04-10 14:10:24 +0200

    add urls and add post to apiview

--- backend/qpd_api/views.py
+++ backend/qpd_api/views.py
@@ -1,9 +1,32 @@
+from urllib import request
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+import json
+
+from qpd_api import serializers
+from qpd_logic import runjob
 
 
 class JobApiView(APIView):
     """Logic for Job Endpoint"""
 
-    def post(self, request, format=None):
+    serializer_class = serializers.JobSerializer
+
+    def post(self, request):
         """Creates Job object"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            print(request.data)
+            # job = json.loads(request.data)
+
+            # runjob(job)
+            return Response(
+                "Job sent.",
+                status=status.HTTP_200_OK,
+            )
+
+        return Response(
+            "Invalid data.",
+            status=status.HTTP_400_BAD_REQUEST,
+        )
