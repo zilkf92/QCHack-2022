255a173205e2441a2cba5505bd4506871f7477ce
Felix Zilk <zilk.felix@gmail.com>
2022-04-10 13:29:03 +0200

    merge all fields to one model & create serializer

--- backend/qpd_api/models.py
+++ backend/qpd_api/models.py
@@ -10,33 +10,9 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 import uuid
 
 
-class Strategy(models.Model):
+class Job(models.Model):
 
-    theta = models.DecimalField(
-        validators=[
-            MinValueValidator(0),
-            MaxValueValidator(1),
-        ],
-        decimal_places=2,
-        max_digits=5,
-        blank=True,
-        null=True,
-    )
-
-    phi = models.DecimalField(
-        validators=[
-            MinValueValidator(0),
-            MaxValueValidator(1),
-        ],
-        decimal_places=2,
-        max_digits=5,
-        blank=True,
-        null=True,
-    )
-
-
-class Settings(models.Model):
-
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     gamma = models.DecimalField(
         validators=[
             MinValueValidator(0),
@@ -47,16 +23,24 @@ class Settings(models.Model):
         blank=True,
         null=True,
     )
-
     qpu = models.BooleanField(default=False)
-
-
-class Job(models.Model):
-
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    settings = models.ForeignKey(
-        "Settings", on_delete=models.CASCADE, blank=True, null=True
+    theta = models.DecimalField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(1),
+        ],
+        decimal_places=2,
+        max_digits=5,
+        blank=True,
+        null=True,
     )
-    strategy = models.ForeignKey(
-        "Strategy", on_delete=models.CASCADE, blank=True, null=True
+    phi = models.DecimalField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(1),
+        ],
+        decimal_places=2,
+        max_digits=5,
+        blank=True,
+        null=True,
     )
